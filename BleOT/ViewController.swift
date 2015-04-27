16f0743eaa6ad073e2699271db0f01fc09985105
Jonathan Fuerst <jf@jofu.org>
2015-04-27 10:14:19 +0200

    Initial Commit

---
+++ BleOT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BleOT
+//
+//  Created by jofu on 27/04/15.
+//  Copyright (c) 2015 ITU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
